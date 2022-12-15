f5e029a3cebcb65a517f0ec166970c9c71a45e41
son <vodanhserver@gmail.com>
2022-12-15 09:21:10 +0700

    commit signin_screen.dart

--- lib/screens/check_sheet_products/bar_code_screen_check.dart
+++ lib/screens/check_sheet_products/bar_code_screen_check.dart
@@ -62,22 +62,25 @@ class _BarCodeScreenCheckState extends State<BarCodeScreenCheck> {
     return Scaffold(
       appBar: AppBar(title: const Text('Phiếu kiểm kho')),
       body: ListView.builder(
-          itemCount: map.length,
-          itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () {
-                setState(() {
-                  map.remove(map.keys.elementAt(index));
-                });
-              },
-              child: Ink(
-                child: ListTile(
-                  title: Text(map.keys.elementAt(index)),
-                  trailing: Text(map.values.elementAt(index).toString()),
-                ),
+        itemCount: map.length,
+        addAutomaticKeepAlives: true,
+        // notification when map is empty
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              setState(() {
+                map.remove(map.keys.elementAt(index));
+              });
+            },
+            child: Ink(
+              child: ListTile(
+                title: Text(map.keys.elementAt(index)),
+                trailing: Text(map.values.elementAt(index).toString()),
               ),
-            );
-          }),
+            ),
+          );
+        },
+      ),
       floatingActionButton: Builder(
         builder: (BuildContext context) {
           return FloatingActionButton(
@@ -85,10 +88,14 @@ class _BarCodeScreenCheckState extends State<BarCodeScreenCheck> {
             //primary color
             backgroundColor: kPrimaryColor,
             tooltip: "Kiểm tra mã vạch",
-            child: const Icon(Icons.camera_alt, color: Vx.white,),
+            child: const Icon(
+              Icons.camera_alt,
+              color: Vx.white,
+            ),
           );
         },
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
     );
   }
